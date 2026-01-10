e24160c82ca082096e5d89a9d276f403e41bf01a
murphiie <sousageovana.r@gmail.com>
2026-01-10 14:04:04 -0300

    Estrutura FastAPI e modelos Pydantic finalizados

---
+++ database.py
@@ -0,0 +1,20 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+# Esta URL será configurada depois no Docker. 
+MONGODB_URL = "mongodb://localhost:27017"
+
+client = AsyncIOMotorClient(MONGODB_URL)
+db = client.newsflow_db
+collection = db.get_collection("artigos")
+
+# Função auxiliar para formatar os documentos do MongoDB (que usam _id) 
+def artigo_helper(artigo) -> dict:
+    return {
+        "id": str(artigo["_id"]),
+        "titulo": artigo["titulo"],
+        "corpo": artigo["corpo"],
+        "autor": artigo["autor"],
+        "categoria": artigo["categoria"],
+        "data_publicacao": artigo["data_publicacao"],
+    }
